12056f5a36761aa779c283cb7904eae1fd7b0074
Melinda <melinda9794@gmail.com>
2017-02-05 01:23:27 -0500

    push

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,7 +5,7 @@ var config = {
     databaseURL: "https://trainscheduler-3757a.firebaseio.com",
     storageBucket: "trainscheduler-3757a.appspot.com",
     messagingSenderId: "774913011279"
-  };
+  }
 firebase.initializeApp(config);
 
 
@@ -17,9 +17,11 @@ var destination = "";
 var nextArrival = "";
 var minutesAway = "";
 
+$("#currentTrainSchedule").empty();
 
 // FUNCTIONS + EVENTS
-$("#addTrain").on("click", function() {
+//$("#addTrain").on("click", function() {
+function addTrainEntry() {
 
     trainName = $("#trainName").val().trim();
     destination = $("#destination").val().trim();
@@ -34,50 +36,54 @@ $("#addTrain").on("click", function() {
           dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
 
+  // Output all of the new information into the Current Train Schedule section
+      $("#currentTrainSchedule").append("<tr>");
+      $("#currentTrainSchedule").append("<td>" + trainName + "</td>");
+      $("#currentTrainSchedule").append("<td>" + destination + "</td>");
+      $("#currentTrainSchedule").append("<td>" + nextArrival + "</td>");
+      $("#currentTrainSchedule").append("<td>" + minutesAway + "</td>");
+      $("#currentTrainSchedule").append("</tr>");
 
-// MAIN PROCESS + INITIAL CODE
-database.ref().on("child_added", function(snapshot) {
-    console.log(childSnapshot.val().trainName);
-    console.log(childSnapshot.val().destination);
-    console.log(childSnapshot.val().nextArrival);
-    console.log(childSnapshot.val().minutesAway);
-
-  // update the variable with data from the database
-  trainName = snapshot.val().trainName;
-  destination = snapshot.val().destination;
-  nextArrival = snapshot.val().nextArrival;
-  minutesAway = snapshot.val().minutesAway;
-
-
- // Capture Button Click
-     $("#runSearch").on("click", function(event) {
-// don't refresh the page
-      event.preventDefault();
-// Replaces the content in the "currentTrainSchedule" div
-      $("#currentTrainSchedule").empty();
-
-// Output all of the new information into the Current Train Schedule section
-      $("#currentTrainSchedule").append("<h1>" + trainName);
-      $("#currentTrainSchedule").append("<h4>" + destination);
-      $("#currentTrainSchedule").append("<h4>" + nextArrival);
-      $("#currentTrainSchedule").append("<h4>" + minutesAway);
-
-// Clear sessionStorage
+    // Clear sessionStorage
       sessionStorage.clear();
 
-// Store all content into sessionStorage
+    // Store all content into sessionStorage
       sessionStorage.setItem("trainName", trainName);
       sessionStorage.setItem("destination", destination);
       sessionStorage.setItem("nextArrival", nextArrival);
       sessionStorage.setItem("minutesAway", minutesAway);
-        });
 
-  // By default display the content from sessionStorage
-      $("#currentTrainSchedule").empty();
-      $("#currentTrainSchedule").append("<h2>" + sessionStorage.getItem("trainName"));
-      $("#currentTrainSchedule").append("<h4>" + sessionStorage.getItem("destination"));
-      $("#currentTrainSchedule").append("<h4>" + sessionStorage.getItem("nextArrival"));
-      $("#currentTrainSchedule").append("<h4>" + sessionStorage.getItem("minutesAway"));
+      // MAIN PROCESS + INITIAL CODE
+      database.ref().on("child_added", function(snapshot) {
+        console.log(snapshot.val().trainName);
+        console.log(snapshot.val().destination);
+        console.log(snapshot.val().nextArrival);
+        console.log(snapshot.val().minutesAway);
+
+      // update the variable with data from the database
+      trainName = snapshot.val().trainName;
+      destination = snapshot.val().destination;
+      nextArrival = snapshot.val().nextArrival;
+      minutesAway = snapshot.val().minutesAway;
+
+      
+    
+
+      
+    });
+
+      $("#frmTrain")[0].reset();
+
+  //});
+}
+
+
+// By default display the content from sessionStorage
+//$("#currentTrainSchedule").empty();
+//$("#currentTrainSchedule").append("<h2>" + sessionStorage.getItem("trainName"));
+//$("#currentTrainSchedule").append("<h4>" + sessionStorage.getItem("destination"));
+//$("#currentTrainSchedule").append("<h4>" + sessionStorage.getItem("nextArrival"));
+//$("#currentTrainSchedule").append("<h4>" + sessionStorage.getItem("minutesAway"));
 
 
 ////////
